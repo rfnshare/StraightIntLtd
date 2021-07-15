3224ede0318431ac5458d9f9740ae96fe7b2b1f9
RFNshare <rfnshare@gmail.com>
2021-07-15 23:09:49 +0600

    Minor problem updated

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView
-
+from django.contrib import messages
 from django.urls import reverse_lazy
 
 from .models import *
@@ -20,6 +20,7 @@ def create_customer(request):
         form = CustomerForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Form submission successful')
             return redirect('customer:customer_list')
     ctx = {'form': form}
     return render(request, 'customer/create_customer.html', ctx)
