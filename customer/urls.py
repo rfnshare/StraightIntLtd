0f4ce8f364d789a49c97e757e299ec8376e4c08c
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2021-07-15 19:57:35 +0600

    all customer work done

--- customer/urls.py
+++ customer/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     # path('', views.ProtectView.as_view(), name='index'),
     path('', views.create_customer, name='create_customer'),
     path('customer_list/', views.CustomerListView.as_view(), name='customer_list'),
-    path('customer_details/<str:pk>/', views.customerDetails, name='customer_details')
+    path('customer_details/<str:pk>/', views.CustomerDetails.as_view(), name='customer_details'),
+    path('customer_edit/<str:pk>/', views.customer_edit.as_view(),name= 'customer_edit'),
+    path('customer_delete/<str:pk>/', views.customer_delete.as_view(), name='customer_delete'),
+    #path('customer_edit/<str:pk>/', views.cuEdit, name='customer_edit'),
+    #path('customer_delete/<str:pk>/', views.cuDelete, name='customer_delete')
+    
 
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,DeleteView,UpdateView
+
+
+from django.urls import reverse_lazy
 
 from .models import *
 # Create your views here.
@@ -47,3 +50,51 @@ def customerDetails(request, pk):
     customer = Customer.objects.get(id=pk)
     form =CustomerForm(instance= customer)
     return render(request, "customer/customer_details.html",{'form':form})
+
+
+class CustomerDetails(DetailView):
+    model = Customer
+    template_name = 'customer/customer_details.html'
+
+
+
+
+"""def cuEdit(request, pk):
+    customer = Customer.objects.get(id=pk)
+    form =CustomerForm(instance= customer)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST,instance = customer)
+        if form.is_valid():
+            form.save()
+            return redirect('customer:customer_list')
+    ctx = {'form': form}
+    return render(request, 'customer/customer_edit.html', ctx)"""
+
+
+class customer_edit(UpdateView):
+    model = Customer
+    template_name = 'customer/customer_edit.html'
+
+    fields = [
+        'name',
+        'phone',
+        'address',
+        'balance'
+    ]
+    success_url = reverse_lazy('customer:customer_list')
+    
+
+"""def cuDelete(request, pk):
+    customer = Customer.objects.get(id=pk)
+    form = CustomerForm()
+    if request.method == 'POST':
+        customer.delete()
+        return redirect('customer:customer_list')
+    ctx = {'form': form}
+    return render(request, 'customer/customer_delete.html', ctx)"""
+
+
+class customer_delete(DeleteView):
+    model = Customer
+    template_name = 'customer/customer_delete.html'
+    success_url = reverse_lazy('customer:customer_list')
