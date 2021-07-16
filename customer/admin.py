c9be57d15caf10274b855110734215ce4cd14558
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2021-07-16 22:49:59 +0600

    admin.py update

--- customer/admin.py
+++ customer/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
 
 
-admin.site.register(Customer)
+#admin.site.register(Customer)
+
+
+@admin.register(Customer)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'name', 'email', 'phone', 'address')
