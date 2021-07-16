c3956927e50955bf587dd3486c2ef72460320985
Abdullahmdshahid <ucabdullahshahid@gmail.com>
2021-07-16 20:01:34 +0600

    Dashboard update, chart update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from customer.models import *
+
+from django.views.generic import TemplateView
 # Create your views here.
 from django.views import View
 
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['qs'] = Customer.objects.all()
+        return context
 
 def index(request):
     return render(request, 'dashboard/dashboard.html')
