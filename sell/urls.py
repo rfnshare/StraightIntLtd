1faea12fab879e6a8b275d80bd1744e067c065d4
RFNshare <rfnshare@gmail.com>
2021-07-17 13:32:25 +0600

    Basic Invoice work

--- sell/urls.py
+++ sell/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.ProtectView.as_view(), name='index'),
     path('create_sell/', views.InvoiceCreateView.as_view(), name='create'),
     path('sell_list/', views.InvoiceListView.as_view(), name='list'),
+    path('pdf/<str:pk>', views.some_view, name='pdf'),
 ]
--- sell/views.py
+++ sell/views.py
@@ -2,11 +2,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
+from customer.models import Customer
 from .models import *
 from .forms import *
 # Create your views here.
 from django.views import View
 from django.views.generic import *
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
 
 
 def index(request):
@@ -28,3 +32,25 @@ class InvoiceCreateView(LoginRequiredMixin, CreateView):
 class InvoiceListView(LoginRequiredMixin, ListView):
     model = Invoice
     template_name = 'sell/sell_list.html'
+
+
+def some_view(request, pk):
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+
+    p.drawString(100, 100, "Sample")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename=pk+'.pdf')
